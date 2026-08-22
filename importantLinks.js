b67afcf2a7162a4d34f8981a11f363021522636e
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-08-22 16:29:14 +0530

    Create importantLinks.js

---
+++ importantLinks.js
@@ -0,0 +1,29 @@
+const importantLinks = [
+
+    {
+        name:
+            "Google Apps Script",
+
+        url:
+            "https://script.google.com/"
+    },
+
+
+    {
+        name:
+            "GitHub",
+
+        url:
+            "https://github.com/"
+    },
+
+
+    {
+        name:
+            "Google Drive",
+
+        url:
+            "https://drive.google.com/"
+    }
+
+];
