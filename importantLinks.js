55b948c1d9592c5df012f2ae5b07d06c53e16e53
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-08-24 19:19:08 +0530

    Add new links to importantLinks.js

--- importantLinks.js
+++ importantLinks.js
@@ -15,7 +15,28 @@ const importantLinks = [
 
         url:
             "https://drive.google.com/file/d/1Kg3TzJdnIHn2B4MQx4DpjVfLof-BAbNW/view"
-    }
+    },
+        {
+        name:
+            "RoboPost",
+
+        url:
+            "https://rakhechamedical.in/robopost/"
+    },
+        {
+        name:
+            "Online order places web ",
+
+        url:
+            "https://rakhechamedical.in/order/"
+    },    
+        {
+        name:
+            "Chequebook ",
+
+        url:
+            "https://rakhechamedical.in/chequebook/"
+    }      
 
 
 
