9b7ad6801dae7a7d302c3314308c23c5b160012c
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-08-26 17:37:20 +0530

    Create sw.js

---
+++ app/sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "Horizon",
+  "short_name": "Horizon",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#0f172a",
+  "icons": [
+    {
+      "src": "intfebi.png",
+      "sizes": "192x192",
+      "type": "https://i.ibb.co/G4DyH3QN/horizon-icon.png"
+    },
+    {
+      "src": "intfebi.png",
+      "sizes": "512x512",
+      "type": "https://i.ibb.co/kgzJb34B/horizon-logo.png"
+    }
+  ]
+}
