b29a28e39ca9c9c6203e0116725b8fd071d7d0b5
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-08-26 16:57:46 +0530

    Add importantLinks.js with resource URLs

---
+++ test/importantLinks.js
@@ -0,0 +1,44 @@
+const importantLinks = [
+
+    {
+        name:
+            "official webpage",
+
+        url:
+            "https://rakhechamedical.in/"
+    },
+
+
+    {
+        name:
+            "RMS product list PDF",
+
+        url:
+            "https://drive.google.com/file/d/1Kg3TzJdnIHn2B4MQx4DpjVfLof-BAbNW/view"
+    },
+        {
+        name:
+            "RoboPost",
+
+        url:
+            "https://rakhechamedical.in/robopost/"
+    },
+        {
+        name:
+            "Online order places web ",
+
+        url:
+            "https://rakhechamedical.in/order/"
+    },    
+        {
+        name:
+            "Chequebook ",
+
+        url:
+            "https://rakhechamedical.in/chequebook/"
+    }      
+
+
+
+
+];
